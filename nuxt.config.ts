31bc6ab22c216c6f20bf4921f5adf7f0c96ef641
Clement <clement.ramoslage@gmail.com>
2023-10-09 18:11:26 +0200

    feat: viewport metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/supabase', '@nuxt/ui'],
   plugins: ['@/plugins/chartjs.ts'],
   devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1'
+    }
+  },
   colorMode: {
     preference: 'light'
   },

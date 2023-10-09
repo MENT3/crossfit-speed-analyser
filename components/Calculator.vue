b785bc0e2c80126980c6392919dab9511d9622fb
Clement <clement.ramoslage@gmail.com>
2023-10-09 18:30:08 +0200

    refactor: change patterns

--- components/Calculator.vue
+++ components/Calculator.vue
@@ -11,7 +11,11 @@ const x = computed(() => Math.round((speed.value - props.intercept) / props.slop
 <template>
   <div class="flex flex-row items-end space-x-4">
     <UFormGroup label="La vitesse" name="username">
-      <UInput :model-value="y" @update:model-value="v => speed = Number(v)">
+      <UInput
+        :model-value="y"
+        pattern="[0-9]*[.,]?[0-9]*"
+        @update:model-value="v => speed = Number(v)"
+      >
         <template #trailing>
           <span class="text-gray-500 dark:text-gray-400 text-xs">m/s</span>
         </template>
@@ -21,7 +25,11 @@ const x = computed(() => Math.round((speed.value - props.intercept) / props.slop
     <UIcon name="i-heroicons-arrows-right-left" class="h-8" />
 
     <UFormGroup label="Le pourcentage" name="username">
-      <UInput :model-value="x" @update:model-value="v => percentage = Number(v)">
+      <UInput
+        :model-value="x"
+        pattern="[0-9]*[.,]?[0-9]*"
+        @update:model-value="v => percentage = Number(v)"
+      >
         <template #trailing>
           <span class="text-gray-500 dark:text-gray-400 text-xs">%</span>
         </template>
